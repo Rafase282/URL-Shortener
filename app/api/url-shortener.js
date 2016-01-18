a8fa9e305905b4eb329108df011ecc48bee47e78
Rafael J. Rodriguez <rafase282@gmail.com>
2016-01-18 00:51:16 +0000

    Working on the db

--- app/api/url-shortener.js
+++ app/api/url-shortener.js
@@ -1,18 +1,37 @@
 'use strict';
 var validUrl = require('valid-url');
-module.exports = function(app) {
+module.exports = function(app, db) {
 
   app.get('/new/:url*', function(req, res) {
-    
     var url = req.url.slice(5);
-    var urlObj = validUrl.isUri(url) ? {
-      "original_url": url,
-      "short_url": process.env.APP_URL + "4"
-    } : {
-      "error": "No short url found for given input"
-    };
-    res.send(urlObj);
+    var urlObj = {};
 
+    if (validUrl.isUri(url)) {
+      urlObj = {
+        "original_url": url,
+        "short_url": process.env.APP_URL + linkGen()
+      };
+      save(urlObj);
+    } else {
+      urlObj = {
+        "error": "No short url found for given input"
+      };
+    }
+    res.send(urlObj);
   });
+
+  function linkGen() {
+    // Generates random number for link
+    var num = Math.floor(100000 + Math.random() * 900000);
+    return num.toString().substring(0, 4);
+  }
+
+  function save(obj, db) {
+    // Save object into db.
+    db.sites.insert(obj, function(err, result) {
+    
+    if(err) throw err;
+    });
+  }
 
 };
