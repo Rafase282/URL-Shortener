c3bb659c1cb927175095d47fb01f7ba9ed66dca0
Rafael J. Rodriguez <rafase282@gmail.com>
2016-01-20 23:04:45 +0000

    Fixed url verification

--- app/api/url-shortener.js
+++ app/api/url-shortener.js
@@ -1,5 +1,4 @@
 'use strict';
-var validUrl = require('valid-url');
 module.exports = function(app, db) {
 
   app.route('/:url')
@@ -68,10 +67,9 @@ module.exports = function(app, db) {
   
   function validateURL(url) {
     // Checks to see if it is an actual url
-    
-    // /^http(s)?\:\/\/.+\.\w+$/gi
-    
-    return validUrl.isUri(url);
+    // Regex from https://gist.github.com/dperini/729294
+    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return regex.test(url);
   }
 
 };
