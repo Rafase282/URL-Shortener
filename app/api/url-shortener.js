2581831ce1e92158dd7624e589ae405afa95533a
LordMathis <namesny.matus@gmail.com>
2017-06-03 16:45:50 +0200

    fixed short link collision bug

--- app/api/url-shortener.js
+++ app/api/url-shortener.js
@@ -19,12 +19,9 @@ module.exports = function(app, db) {
     var url = req.url.slice(5);
     var urlObj = {};
     if (validateURL(url)) {
-      urlObj = {
-        "original_url": url,
-        "short_url": process.env.APP_URL + linkGen()
-      };
-      res.send(urlObj);
-      save(urlObj, db);
+
+      // Generate link, save in db and send response
+      linkGen(url, res, db, save);
     } else {
       urlObj = {
         "error": "Wrong url format, make sure you have a valid protocol and real site."
@@ -33,17 +30,49 @@ module.exports = function(app, db) {
     }
   }
 
-  function linkGen() {
-    // Generates random four digit number for link
-    var num = Math.floor(100000 + Math.random() * 900000);
-    return num.toString().substring(0, 4);
+  function linkGen(url, res, db, callback) {
+    // Gather all existing short links
+    db.collection('sites').find().toArray((err, data) => {
+      if (err) return callback(err);
+
+      // Put all short links in an array
+      var urlList = data.map((obj) => {
+        return obj.short_url;
+      });
+
+      var newLink;
+      // Generate link and check for uniqueness
+      do {
+        // Generates random four digit number for link
+        var num = Math.floor(100000 + Math.random() * 900000);
+        newLink = process.env.APP_URL + num.toString().substring(0, 4);
+      } while (urlList.indexOf(newLink) != -1);
+
+      return callback(null, url, newLink, res, db);
+    });
   }
 
-  function save(obj, db) {
+  function save(err, url, newLink, res, db) {
+    if (err) throw err;
+
+    // Create new object
+    var urlObj = {
+      "original_url": url,
+      "short_url": newLink
+    };
+
     // Save object into db.
     var sites = db.collection('sites');
-    sites.save(obj, function(err, result) {
+    sites.save(urlObj, function(err, result) {
       if (err) throw err;
+
+      // Send response object
+      // We need to create the object again because
+      // urlObj now contains database id
+      res.send({
+        "original_url": url,
+        "short_url": newLink
+      });
       console.log('Saved ' + result);
     });
   }
